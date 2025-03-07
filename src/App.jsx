219f145c120672c3cb015ab5f5efdd2edd09cfac
Preetham Goud <preethamsureshgoud@gmail.com>
2025-03-07 13:18:33 -0500

    "Created MovieCard component boilerplate"

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import MovieCard from "./components/MovieCard";
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <MovieCard movie={{ title: "RRR", release_date: "2022" }} />
+      <MovieCard movie={{ title: "Barbie", release_date: "2023" }} />
+      <MovieCard movie={{ title: "Gentlemen", release_date: "2020" }} />
+    </>
+  );
 }
 
 export default App;
