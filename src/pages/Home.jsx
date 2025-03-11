870b41689454df843264a7b2b7c16d378f9f395d
Preetham Goud <preethamsureshgoud@gmail.com>
2025-03-11 11:25:29 -0400

    "Added search functionality with state to refresh component on change of search string"

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,20 @@
 import MovieCard from "../components/MovieCard";
+import { useState } from "react";
 
 function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+
   const movies = [
     { id: 1, title: "Dune", release_date: "2021" },
     { id: 2, title: "Avatar", release_date: "2009" },
     { id: 3, title: "The Avengers", release_date: "2012" },
   ];
 
-  const handleSearch = () => {};
+  const handleSearch = (e) => {
+    e.preventDefault();
+    alert(searchQuery);
+    setSearchQuery("");
+  };
 
   return (
     <div className="home">
@@ -16,15 +23,20 @@ function Home() {
           type="text"
           placeholder="Search for movies..."
           className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
         />
         <button type="submit" className="search-button">
           Search
         </button>
       </form>
       <div className="movies-grid">
-        {movies.map((movie) => (
-          <MovieCard movie={movie} key={movie.id} />
-        ))}
+        {movies.map(
+          (movie) =>
+            movie.title.toLowerCase().includes(searchQuery) && (
+              <MovieCard movie={movie} key={movie.id} />
+            )
+        )}
       </div>
     </div>
   );
