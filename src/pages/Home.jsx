cc4ecb9dca7abb3df36f903de51f44e402693fb8
Preetham Goud <preethamsureshgoud@gmail.com>
2025-03-11 13:18:58 -0400

    "API Integration"

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,28 @@
 import MovieCard from "../components/MovieCard";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { searchMovies, getPopularMovies } from "../services/api";
 import "../css/Home.css";
 
 function Home() {
   const [searchQuery, setSearchQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  const movies = [
-    { id: 1, title: "Dune", release_date: "2021" },
-    { id: 2, title: "Avatar", release_date: "2009" },
-    { id: 3, title: "The Avengers", release_date: "2012" },
-  ];
+  useEffect(() => {
+    const loadPopularMovies = async () => {
+      try {
+        const popularMovies = await getPopularMovies();
+        setMovies(popularMovies);
+      } catch (err) {
+        setError("Failed to load movies...");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadPopularMovies();
+  }, []);
 
   const handleSearch = (e) => {
     e.preventDefault();
