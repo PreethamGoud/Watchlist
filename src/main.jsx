b806fa5222012e6410882c05be54aa18e440dede
Preetham Goud <preethamsureshgoud@gmail.com>
2025-03-11 23:07:48 -0400

    "deployment fixes"

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./css/index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <Router basename="/Watchlist">
       <App />
-    </BrowserRouter>
+    </Router>
   </StrictMode>
 );
